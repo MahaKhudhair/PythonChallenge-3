1622a40f758167af7ec4137004ce76b0a38017d4
MahaKhudhair <41030892+MahaKhudhair@users.noreply.github.com>
2018-09-10 01:52:25 +0300

    Add files via upload

---
+++ Q2.py
@@ -0,0 +1,8 @@
+Python 3.7.0b4 (v3.7.0b4:eb96c37699, May  2 2018, 19:02:22) [MSC v.1913 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> Types= ['Car', 'Device', 'Fruit', 'Sport']
+>>> Things= ['Apple', 'Tennis', 'Computer', 'Mercedes']
+>>> myDict= {Things[0] :Types[2] , Things[2] :Types[1] , Things[3] :Types[0] , Things[1] :Types[3] }
+>>> print (myDict)
+{'Apple': 'Fruit', 'Computer': 'Device', 'Mercedes': 'Car', 'Tennis': 'Sport'}
+>>> 
